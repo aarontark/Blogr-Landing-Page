4815be4dc34ffa312f7ff1b618fb86b0c69e1a51
aarontark <tarkington.aaron@gmail.com>
2024-07-21 05:51:42 -0500

    made desktop buttons responsive

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ navMenuItems.forEach((e) => {
     e.style.display = 'none';
 })
 
-alert(window.innerHeight);
-alert(window.innerWidth);
-
 hamburgerIcon.addEventListener('click', () => {
     if(hamburgerIcon.classList.contains('hamburger-icon')){
         hamburgerIcon.classList.remove('hamburger-icon');
